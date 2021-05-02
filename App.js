274400750ba0adf5631b2b46448830ce90bd5ce9
kritvikalani <kritvikalani@woodstock.ac.in>
2021-05-02 18:15:42 +0530

    story hub

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {createAppContainer} from 'react-native';
-import {createBottomTabNavigator} from 'react-native-tabs';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import WriteStoryScreen from './screens/WriteStoryScreen'
 import ReadStoryScreen from './screens/ReadStoryScreen'
